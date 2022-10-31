a5525b8ea35b25fa136b3fc27dd83d7c3fb76044
Evgenij-Kovpak <58892723+Evgenij-Kovpak@users.noreply.github.com>
2022-10-31 19:31:29 +0200

    Add project files.

---
+++ Module3Practise4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module3Practise4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
